1ec27720452f3293c28127eb15d828c1f346f94b
e.ghosi <e.ghosi@processingworld.com>
2025-07-08 14:24:29 +0330

    Routing - Direct

---
+++ EmitLogsDirect/EmitLogsDirect.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { 
+    HostName = "192.168.106.121",
+    UserName = "user1",
+    Password = "user1"
+ };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(exchange: "direct_logs", type: ExchangeType.Direct);
+
+var severity = (args.Length > 0) ? args[0] : "info";
+var message = (args.Length > 1) ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+await channel.BasicPublishAsync(exchange: "direct_logs", routingKey: severity, body: body);
+Console.WriteLine($" [x] Sent '{severity}':'{message}'");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
