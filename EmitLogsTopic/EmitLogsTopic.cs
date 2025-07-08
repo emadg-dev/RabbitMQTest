8ab6ab87c8765ce651014c1aaf74814ab9d553af
e.ghosi <e.ghosi@processingworld.com>
2025-07-08 15:32:30 +0330

    Routing - Topic

---
+++ EmitLogsTopic/EmitLogsTopic.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { 
+    HostName = "192.168.106.121",
+    UserName = "user1",
+    Password = "user1"
+ };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(exchange: "topic_logs", type: ExchangeType.Topic);
+
+var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
+var message = (args.Length > 1) ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+await channel.BasicPublishAsync(exchange: "topic_logs", routingKey: routingKey, body: body);
+Console.WriteLine($" [x] Sent '{routingKey}':'{message}'");
+
+// Console.WriteLine(" Press [enter] to exit.");
+// Console.ReadLine();
