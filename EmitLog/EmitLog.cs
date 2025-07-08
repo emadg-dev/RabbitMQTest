667ce73ccad5dce54e83969b309eb59b723b4f32
e.ghosi <e.ghosi@processingworld.com>
2025-07-08 11:15:05 +0330

    Log Publish Subscribe

---
+++ EmitLog/EmitLog.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { 
+    HostName = "192.168.106.121",
+    UserName = "user1",
+    Password = "user1"
+ };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(exchange: "logs", type: ExchangeType.Fanout);
+
+var message = GetMessage(args);
+var body = Encoding.UTF8.GetBytes(message);
+await channel.BasicPublishAsync(exchange: "logs", routingKey: string.Empty, body: body);
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "info: Hello World!");
+}
